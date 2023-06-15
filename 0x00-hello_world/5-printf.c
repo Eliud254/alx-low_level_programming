a4f7571d219a6e772c0a55afbc4702919eebcc13
Eliud254 <eliudobure@gmail.com>
2023-06-15 15:24:29 -0700

    100-intel

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -with proper grammar, but the outcome is a piece of art
+ * Return: return to 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
