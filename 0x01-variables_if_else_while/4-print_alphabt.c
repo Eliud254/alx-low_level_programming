1ee5474a5033426bfc11457162f58111b7569214
Eliud254 <eliudobure@gmail.com>
2023-06-16 07:23:00 -0700

    4-alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase,
+ * followed by a new line 
+ *Except q and q and e
+ * Return: 0 (Sucess)
+ */
+int main(void)
+{
+char ch = 'a';
+while (ch <= 'z') 
+{       
+if (ch != 'e' && ch != 'q') 
+{       
+putchar(ch);
+}       
+ch++;   
+}       
+putchar('\n');
+return (0);
+}
