0da8872b68ca3ffd157dc3ebd87a0a0a0dd48f86
Eliud254 <eliudobure@gmail.com>
2023-06-16 09:56:06 -0700

    100-print_comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,21 +6,21 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{
+	{	
 int x, y, z;
 for (x = 48; x < 58; x++)
-{       
+	{       
 for (y = 49; y < 58; y++)
-{       
+	{       
 for (z = 50; z < 58; z++)
-{       
+	{       
 if (z > y && y> x)
-{       
+	{       
 	putchar(x);
 	putchar(y);
 	putchar(z);
 if (x != 55 || y!= 56)
-{       
+	{       
 	putchar(',');
 	putchar(' ');
 }       
