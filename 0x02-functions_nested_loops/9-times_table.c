b6aff3cad254bef917c3df1cd27fc55f74579f3f
Eliud254 <eliudobure@gmail.com>
2023-06-20 16:20:54 -0700

    Time table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * prints the 9 times table, starting with 0
+ */
+
+void times_table(void)
+{
+    int j, k, l;
+
+    for (j = 0; j <= 9; j++) {
+        for (k = 0; k <= 9; k++) {
+            l = j * k;
+
+            if (l > 9) {
+                _putchar(l / 10 + '0');
+                _putchar(l % 10 + '0');
+            } else {
+                _putchar(l + '0');
+            }
+
+            if (k != 9) {
+                _putchar(',');
+                _putchar(' ');
+            }
+        }
+        _putchar('\n');
+    }
+}
+
