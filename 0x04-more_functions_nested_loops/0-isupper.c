c633d01bec715202b02950e591f755f4120c7554
Eliud254 <eliudobure@gmail.com>
2023-06-22 03:24:13 -0700

    0-isuper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * _isdigit - check if numbers are 0 - 9
- * @c: char to be checked
+ * _isupper - uppercase letters
+ * @c: char to check
  *
  * Return: 0 or 1
  */
 
-int _isdigit(int c)
+int _isupper(int c)
 {
-    if (c >= '0' && c <= '9')
-        return (1);
-    else
-        return (0);
+	if (c >= 'A' && c <= 'Z')
+return (1);
+	else
+		return (0);
 }
