242cba69ccacbc0e799f5afa2110d56de5a8d426
Eliud254 <eliudobure@gmail.com>
2023-07-13 07:01:45 -0700

    more malloc

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,106 +1,88 @@
-#ifndef MAIN_H
-#define MAIN_H
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
-#include <unistd.h>
-
-int _putchar(char c);
-void *malloc_checked(unsigned int b);
-char *string_nconcat(char *s1, char *s2, unsigned int n);
-void *_calloc(unsigned int nmemb, unsigned int size);
-int *array_range(int min, int max);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-void errors(void);
-int _strlen(char *s);
-int is_digit(char *s);
-
-#endif
+#include <ctype.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * multiply - Multiplies two positive numbers
+ * @num1: First number
+ * @num2: Second number
  */
-int _putchar(char c)
+void multiply(char *num1, char *num2)
 {
-    return (write(1, &c, 1));
+	int len1, len2, len, i, j, carry;
+	int *result;
+
+	len1 = 0;
+	while (num1[len1])
+	{
+		if (!isdigit(num1[len1]))
+		{
+			printf("Error\n");
+			exit(98);
+		}
+		len1++;
+	}
+
+	len2 = 0;
+	while (num2[len2])
+	{
+		if (!isdigit(num2[len2]))
+		{
+			printf("Error\n");
+			exit(98);
+		}
+		len2++;
+	}
+
+	len = len1 + len2;
+	result = calloc(len, sizeof(int));
+	if (result == NULL)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	for (i = len1 - 1; i >= 0; i--)
+	{
+		carry = 0;
+		for (j = len2 - 1; j >= 0; j--)
+		{
+			carry += (num1[i] - '0') * (num2[j] - '0') + result[i + j + 1];
+			result[i + j + 1] = carry % 10;
+			carry /= 10;
+		}
+		if (carry > 0)
+			result[i + j + 1] += carry;
+	}
+
+	i = 0;
+	while (result[i] == 0 && i < len - 1)
+		i++;
+
+	for (; i < len; i++)
+		printf("%d", result[i]);
+	printf("\n");
+
+	free(result);
 }
 
-int is_digit(char *s)
-{
-    int i = 0;
-
-    while (s[i])
-    {
-        if (s[i] < '0' || s[i] > '9')
-            return (0);
-        i++;
-    }
-    return (1);
-}
-
-int _strlen(char *s)
-{
-    int i = 0;
-
-    while (s[i] != '\0')
-    {
-        i++;
-    }
-    return (i);
-}
-
-void errors(void)
-{
-    printf("Error\n");
-    exit(98);
-}
-
+/**
+ * main - Entry point
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line arguments
+ *
+ * Return: 0 on success, 98 on error
+ */
 int main(int argc, char *argv[])
 {
-    char *s1, *s2;
-    int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (98);
+	}
 
-    s1 = argv[1], s2 = argv[2];
-    if (argc != 3 || !is_digit(s1) || !is_digit(s2))
-        errors();
-    len1 = _strlen(s1);
-    len2 = _strlen(s2);
-    len = len1 + len2 + 1;
-    result = malloc(sizeof(int) * len);
-    if (!result)
-        return (1);
-    for (i = 0; i <= len1 + len2; i++)
-        result[i] = 0;
-    for (len1 = len1 - 1; len1 >= 0; len1--)
-    {
-        digit1 = s1[len1] - '0';
-        carry = 0;
-        for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
-        {
-            digit2 = s2[len2] - '0';
-            carry += result[len1 + len2 + 1] + (digit1 * digit2);
-            result[len1 + len2 + 1] = carry % 10;
-            carry /= 10;
-        }
-        if (carry > 0)
-            result[len1 + len2 + 1] += carry;
-    }
-    for (i = 0; i < len - 1; i++)
-    {
-        if (result[i])
-            a = 1;
-        if (a)
-            _putchar(result[i] + '0');
-    }
-    if (!a)
-        _putchar('0');
-    _putchar('\n');
-    free(result);
-    return (0);
+	multiply(argv[1], argv[2]);
+
+	return (0);
 }
 
