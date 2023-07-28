09db61ba52faaa46a1e06759ebd14f4266d965b1
Eliud254 <eliudobure@gmail.com>
2023-07-27 18:04:03 -0700

    Betty free error code

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,31 +11,30 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
- list_t *new;
- list_t *temp = *head;
- unsigned int len = 0;
+list_t *new;
+list_t *temp = *head;
+unsigned int len = 0;
 
- while (str[len])
- len++;
+while (str[len])
+len++;
 
- new = malloc(sizeof(list_t));
- if (!new)
- return (NULL);
+new = malloc(sizeof(list_t));
+if (!new)
+return (NULL);
 
- new->str = strdup(str);
- new->len = len;
- new->next = NULL;
+new->str = strdup(str);
+new->len = len;
+new->next = NULL;
 
- if (*head == NULL)
- {
- *head = new;
- return (new);
- }
+if (*head == NULL)
+{
+*head = new;
+return (new);
+}
 
- while (temp->next)
- temp = temp->next;
+while (temp->next)
+temp = temp->next;
 
- temp->next = new;
-
- return (new);
+temp->next = new;
+return (new);
 }
