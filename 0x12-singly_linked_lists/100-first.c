3713248955b42592689463817dd140f096b10701
Eliud254 <eliudobure@gmail.com>
2023-07-27 17:39:08 -0700

    100-first

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 
-void first(void) __attribute__ ((constructor));
+void first(void) __attribute__((constructor));
 
 /**
- * first - prints a sentence before the main
- * function is executed
+ * first - Function executed automatically before main()
+ *
+ * This function is automatically executed before the main function
+ * is called when the program starts. It prints a sentence to the
+ * standard output to showcase its execution.
  */
 void first(void)
 {
- printf("You're beat! and yet, you must allow,\n");
- printf("I bore my house upon my back!\n");
+    printf("You're beat! and yet, you must allow,\n");
+    printf("I bore my house upon my back!\n");
 }
+
