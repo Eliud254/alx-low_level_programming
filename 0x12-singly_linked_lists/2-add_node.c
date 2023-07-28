1f7a6f63269097dab8aa27b1d8e1c8b7981fc963
Eliud254 <eliudobure@gmail.com>
2023-07-27 18:09:37 -0700

    Betty free code correction

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,21 +11,20 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new;
-    unsigned int len = 0;
+list_t *new;
+unsigned int len = 0;
 
-    while (str[len])
-        len++;
+while (str[len])
+len++;
 
-    new = malloc(sizeof(list_t));
-    if (!new)
-        return (NULL);
+new = malloc(sizeof(list_t));
+if (!new)
+return (NULL);
 
-    new->str = strdup(str);
-    new->len = len;
-    new->next = (*head);
-    (*head) = new;
+new->str = strdup(str);
+new->len = len;
+new->next = (*head);
+(*head) = new;
 
-    return (*head);
+return (*head);
 }
-
