c947a48c267817030340b206cfb1fd5f722f2f34
Eliud254 <eliudobure@gmail.com>
2023-07-31 14:32:15 -0700

    betty free

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,15 +8,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-    size_t num_nodes = 0;
+size_t num_nodes = 0;
+while (h)
+{
+printf("%d\n", h->n);
+num_nodes++;
+h = h->next;
+}
 
-    while (h)
-    {
-        printf("%d\n", h->n);
-        num_nodes++;
-        h = h->next;
-    }
-
-    return (num_nodes);
+return (num_nodes);
 }
 
