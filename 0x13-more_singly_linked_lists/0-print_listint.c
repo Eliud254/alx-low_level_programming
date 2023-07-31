151c806edcf0b06585a5278683071822a5ec19ce
Eliud254 <eliudobure@gmail.com>
2023-07-31 14:35:17 -0700

    Correction

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,15 @@
  */
 size_t print_listint(const listint_t *h)
 {
-size_t num_nodes = 0;
-while (h)
-{
-printf("%d\n", h->n);
-num_nodes++;
-h = h->next;
-}
+	size_t num_nodes = 0;
 
-return (num_nodes);
+	while (h)
+	{
+		printf("%d\n", h->n);
+		num_nodes++;
+		h = h->next;
+	}
+
+	return (num_nodes);
 }
 
