213fc53e62094008acdf00d780e5967b52b003b4
Eliud254 <eliudobure@gmail.com>
2023-08-02 15:47:00 -0700

    Commits

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -34,7 +34,8 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
-
-
+listint_t *add_nodeint(listint_t **head, int n);
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, int n)
 #endif
 
