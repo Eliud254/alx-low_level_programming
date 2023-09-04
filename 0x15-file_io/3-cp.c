ae72a835930d498b79947ceff9aa8fd81099a6c9
Eliud254 <eliudobure@gmail.com>
2023-09-04 16:47:23 -0700

    Correction

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,80 +1,83 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
- * error_file - Checks if files can be opened.
+ * error_file - Checks if files can be opened and exits
+ *with an error message if not.
  * @file_from: File descriptor for the source file.
  * @file_to: File descriptor for the destination file.
  * @argv: Arguments vector.
  *
- * This function checks if files can be opened and exits the program with
+ * This function checks if files can be opened and
+ * exits the program with
  * an error message if an open operation fails.
  */
 void error_file(int file_from, int file_to, char *argv[])
 {
-    if (file_from == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-        exit(98);
-    }
-    if (file_to == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-        exit(99);
-    }
+if (file_from == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(EXIT_FAILURE);
+}
+if (file_to == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+exit(EXIT_FAILURE);
+}
 }
 
 /**
  * main - Copies the contents of one file to another.
  * @argc: Number of command-line arguments.
- * @argv: Arguments vector.
+ * @argv: Arguments vector of the file.
  *
  * Return: Always returns 0 on success.
  *
- * Description: Exits with specific error codes in case of incorrect arguments
- *              or file operation failures.
+ * Description: Exits with specific error codes
+ *in case of incorrect arguments
+ *or file operation failures.
  */
 int main(int argc, char *argv[])
 {
-    int file_from, file_to, err_close;
-    ssize_t nchars, nwr;
-    char buf[1024];
+int source_fd, dest_fd, close_result;
+ssize_t read_chars, write_chars;
+char buffer[1024];
 
-    if (argc != 3)
-    {
-        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-        exit(97);
-    }
-
-    file_from = open(argv[1], O_RDONLY);
-    file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-    error_file(file_from, file_to, argv);
-
-    nchars = 1024;
-    while (nchars == 1024)
-    {
-        nchars = read(file_from, buf, 1024);
-        if (nchars == -1)
-            error_file(-1, 0, argv);
-        nwr = write(file_to, buf, nchars);
-        if (nwr == -1)
-            error_file(0, -1, argv);
-    }
-
-    err_close = close(file_from);
-    if (err_close == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-        exit(100);
-    }
-
-    err_close = close(file_to);
-    if (err_close == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
-        exit(100);
-    }
-
-    return (0);
+if (argc != 3)
+{
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+exit(EXIT_FAILURE);
 }
 
+source_fd = open(argv[1], O_RDONLY);
+dest_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+error_file(source_fd, dest_fd, argv);
+
+read_chars = 1024;
+while (read_chars == 1024)
+{
+read_chars = read(source_fd, buffer, 1024);
+if (read_chars == -1)
+error_file(-1, 0, argv);
+write_chars = write(dest_fd, buffer, read_chars);
+if (write_chars == -1)
+error_file(0, -1, argv);
+}
+
+close_result = close(source_fd);
+if (close_result == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close source file descriptor\n");
+exit(EXIT_FAILURE);
+}
+
+close_result = close(dest_fd);
+if (close_result == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close destination file descriptor\n");
+exit(EXIT_FAILURE);
+}
+
+return (EXIT_SUCCESS);
+}
