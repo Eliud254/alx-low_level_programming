0bb9ac78b1c0e6cdec6259828b12215a3156c980
Eliud254 <eliudobure@gmail.com>
2023-10-13 15:39:35 -0700

    python project

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_dlistint - The linked dlistint_t list.
+ * @head:Dlistint_t list head
+ * */
+void free_dlistint(dlistint_t *head)
+{
+dlistint_t *tmp;
+
+while (head)
+{
+tmp = head->next;
+free(head);
+head = tmp;   
+}
+}
